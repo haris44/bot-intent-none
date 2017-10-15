0f74d4e8544ba87da70f8720ed28480341861f6a
Alexandre <lxndr.bertrand@gmail.com>
2017-10-15 22:40:07 +0200

    re-send all intent object

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const connexionEstablished = connexion()
 
 assertQueue(connexionEstablished, ($message) => {
     const newMessage = Object.assign($message.message, { content: 'Je n\'ai malheuresement pas compris ton message' })
+    const newIntentMessage = Object.assign($message, { message: newMessage })
     sendTo(connexionEstablished, JSON.stringify(newMessage))
 })
