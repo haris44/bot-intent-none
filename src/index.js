38cabb77629d17d964cfdd74f4c350a188cded9d
Alexandre BERTRAND <lxndr.bertrand@gmail.com>
2017-10-16 10:34:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ const connexionEstablished = connexion()
 assertQueue(connexionEstablished, ($message) => {
     const newMessage = Object.assign($message.message, { content: 'Je n\'ai malheuresement pas compris ton message' })
     const newIntentMessage = Object.assign($message, { message: newMessage })
-    sendTo(connexionEstablished, JSON.stringify(newMessage))
+    sendTo(connexionEstablished, JSON.stringify(newIntentMessage))
 })
