5c26c1f7eeae370b3f7454443c9d9ac6972e317d
Alexandre <lxndr.bertrand@gmail.com>
2018-05-17 22:13:39 +0200

    fix test

--- test/message.js
+++ test/message.js
@@ -3,6 +3,8 @@ import { logic } from '../src/logic'
 
 test(t => {
     return logic({ message: { content: 'taGrandeRace' } }).then(element => {
-        t.true(element.content === 'Je n\'ai malheuresement pas compris ton message')
+        t.deepEqual(element[0].content, 'Je n\'ai malheuresement pas compris ton message')
+        t.deepEqual(element[1].content, "Mais n'hésite pas à réessayer avec une autre formulation !")
+
     })
 })
